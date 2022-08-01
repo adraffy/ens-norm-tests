b28b5c35c81562b9b80e4151909abfcd188ea588
Andrew Raffensperger <raffy@me.com>
2022-08-01 16:29:29 -0700

    remove unnecessary async, add impl dump

--- eth-labels/db.js
+++ eth-labels/db.js
@@ -1,2 +1,2 @@
-import {readFile} from 'node:fs/promises';
-export default JSON.parse(await readFile(new URL('./db.json', import.meta.url)));
+import {readFileSync} from 'node:fs';
+export default JSON.parse(readFileSync(new URL('./db.json', import.meta.url)));
--- impls.js
+++ impls.js
@@ -1,7 +1,7 @@
-import {readFile} from 'node:fs/promises';
+import {readFileSync} from 'node:fs';
 
-async function read_package_version(path) {
-	return JSON.parse(await readFile(new URL(`./node_modules/${path}/package.json`, import.meta.url))).version;
+function read_package_version(path) {
+	return JSON.parse(readFileSync(new URL(`./node_modules/${path}/package.json`, import.meta.url))).version;
 }
 
 export const IMPLS = [];
@@ -13,7 +13,7 @@ export const ensdomains = lib.normalize.bind(lib);
 IMPLS.push({
 	name: 'eth-ens-namehash', 
 	fn: ensdomains, 
-	version: await read_package_version('@ensdomains/eth-ens-namehash')
+	version: read_package_version('@ensdomains/eth-ens-namehash')
 });
 
 // ********************************************************************************
@@ -23,7 +23,7 @@ export {reference};
 IMPLS.push({
 	name: 'Reference', 
 	fn: reference, 
-	version: await read_package_version(`@adraffy/ens-norm-ref-impl`)
+	version: read_package_version(`@adraffy/ens-norm-ref-impl`)
 });
 
 // ********************************************************************************
@@ -33,14 +33,14 @@ export {ens_normalize};
 IMPLS.push({
 	name: 'ens_normalize', 
 	fn: ens_normalize, 
-	version: await read_package_version(`@adraffy/ens-normalize`)
+	version: read_package_version(`@adraffy/ens-normalize`)
 });
 
 // ********************************************************************************
 
 import {create_uts46} from '@adraffy/ens-norm-uts46';
-const UTS46_VERSION = await read_package_version(`@adraffy/ens-norm-uts46`);
-export const uts46 = await create_uts46({
+const UTS46_VERSION = read_package_version(`@adraffy/ens-norm-uts46`);
+export const uts46 = create_uts46({
 	version: 2003, 
 	valid_deviations: true,
 	check_hyphens: true,
@@ -55,7 +55,7 @@ IMPLS.push({
 	version: UTS46_VERSION
 });
 
-export const ens0 = await create_uts46({
+export const ens0 = create_uts46({
 	version: 2003,
 	valid_deviations: true,
 	check_hyphens: true,
@@ -67,7 +67,7 @@ IMPLS.push({
 	version: UTS46_VERSION
 });
 
-export const strict2008 = await create_uts46({
+export const strict2008 = create_uts46({
 	version: 2008, 
 	check_hyphens: true,
 	check_bidi: true,
@@ -86,4 +86,24 @@ IMPLS.push({
 
 for (let impl of IMPLS) {
 	impl.slug = impl.name.replace(/\s+/, '').toLowerCase();
+}
+
+// ********************************************************************************
+
+// dump out if run directly
+if (process.argv[1] === new URL(import.meta.url).pathname) {
+	let cols = ['#', 'name', 'slug', 'version'];
+	let rows = IMPLS.map((impl, i) => cols.map(key => {
+		if (key == '#') return String(i+1);
+		return impl[key];
+	}));
+	let maxs = cols.map((_, i) => rows.reduce((a, row) => Math.max(a, row[i].length), cols[i].length));
+	function format(v, pad = ' ', sep = ' | ') {
+		return cols.map((_, i) => String(v[i]).padEnd(maxs[i], pad)).join(sep);
+	}
+	console.log(format(cols));
+	console.log(format(cols.map(() => ''), '-', '-|-')); 
+	for (let row of rows) {
+		console.log(format(row));
+	}
 }
