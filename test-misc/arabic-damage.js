db5886004d3495970c609319ef170819e7defc04
Andrew Raffensperger <raffy@me.com>
2022-08-02 01:00:33 -0700

    update arabic-damage again

--- test-misc/arabic-damage.js
+++ test-misc/arabic-damage.js
@@ -3,38 +3,40 @@
 import {reference} from '../impls.js';
 import LABELS from '../eth-labels/db.js';
 import {mkdirSync, writeFileSync} from 'node:fs';
+import {escape_unicode} from '../utils.js';
 
 let out_dir = new URL('./output/', import.meta.url);
 mkdirSync(out_dir, {recursive: true});
 
-const BEFORE = /[\u{6F0}-\u{6F3}\u{6F7}-\u{6F9}]/u;
-const MAPPED = /[\u{660}-\u{663}\u{667}-\u{669}]/u;
+const MAPPED = /[\u{6F0}-\u{6F3}\u{6F7}-\u{6F9}]/u;
+const PROPER = /[\u{660}-\u{663}\u{667}-\u{669}]/u;
 
 let valid = [];
 let invalid = [];
 let tally = {};
 for (let label of LABELS) {
-	let before = BEFORE.test(label);
 	let mapped = MAPPED.test(label);
-	if (before || mapped) {
+	let proper = PROPER.test(label);
+	if (mapped || proper) {
+		let escaped = escape_unicode(label);
 		try {
 			let norm = reference(label);
-			tally[norm] = (tally[norm] ?? 0) + 1;
-			if (before) valid.push(label);
+			let v = tally[norm];
+			if (!v) tally[norm] = v = [];
+			v.push(escaped);
+			if (mapped) valid.push(escaped);
 		} catch (err) {
-			if (before) invalid.push(label);
+			if (mapped) invalid.push(escaped);
 		}
 	}
 }
 
-let total = valid.length + invalid.length;
-let both = Object.entries(tally).filter(x => x[1] > 1);
+let both = Object.entries(tally).map(x => x[1]).filter(x => x.length > 1).sort((a, b) => b.length - a.length);
 
 console.log({
-	total,
 	valid: valid.length,
 	invalid: invalid.length,
 	both: both.length
 });
 
-writeFileSync(new URL('./arabic-damage.json', out_dir), JSON.stringify({total, valid, invalid, both}, null, '\t'));
+writeFileSync(new URL('./arabic-damage.json', out_dir), JSON.stringify({valid, invalid, both}, null, '\t'));
