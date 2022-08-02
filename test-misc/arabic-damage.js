d23d05b62597f7c325aeb8515489e6ca4f134ea5
Andrew Raffensperger <raffy@me.com>
2022-08-02 00:53:30 -0700

    update arabic-damage with dups

--- test-misc/arabic-damage.js
+++ test-misc/arabic-damage.js
@@ -7,27 +7,34 @@ import {mkdirSync, writeFileSync} from 'node:fs';
 let out_dir = new URL('./output/', import.meta.url);
 mkdirSync(out_dir, {recursive: true});
 
-let regex = /[\u{6F0}-\u{6F3},\u{6F7}-\u{6F9}]/u;
+const BEFORE = /[\u{6F0}-\u{6F3}\u{6F7}-\u{6F9}]/u;
+const MAPPED = /[\u{660}-\u{663}\u{667}-\u{669}]/u;
 
 let valid = [];
 let invalid = [];
+let tally = {};
 for (let label of LABELS) {
-	if (regex.test(label)) {
+	let before = BEFORE.test(label);
+	let mapped = MAPPED.test(label);
+	if (before || mapped) {
 		try {
-			reference(label);
-			valid.push(label);
+			let norm = reference(label);
+			tally[norm] = (tally[norm] ?? 0) + 1;
+			if (before) valid.push(label);
 		} catch (err) {
-			invalid.push(label);
+			if (before) invalid.push(label);
 		}
 	}
 }
 
 let total = valid.length + invalid.length;
+let both = Object.entries(tally).filter(x => x[1] > 1);
 
 console.log({
 	total,
 	valid: valid.length,
-	invalid: invalid.length
+	invalid: invalid.length,
+	both: both.length
 });
 
-writeFileSync(new URL('./arabic-damage.json', out_dir), JSON.stringify({total, valid, invalid}, null, '\t'));
+writeFileSync(new URL('./arabic-damage.json', out_dir), JSON.stringify({total, valid, invalid, both}, null, '\t'));
