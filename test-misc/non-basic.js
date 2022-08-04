208ffc84b0d4c888873692b9130e39c731871474
Andrew Raffensperger <raffy@me.com>
2022-08-03 23:28:03 -0700

    include emoji as basic

--- test-misc/non-basic.js
+++ test-misc/non-basic.js
@@ -1,23 +1,37 @@
 // count non-basic
 
 import LABELS from '../eth-labels/db.js';
-import {reference} from '../impls.js';
+import {escape_unicode} from '@adraffy/ens-norm-uts46';
+import {ens_normalize, ens_tokenize} from '@adraffy/ens-normalize';
 import {mkdirSync, writeFileSync} from 'node:fs';
-import { escape_unicode } from '@adraffy/ens-norm-uts46';
 
 let out_dir = new URL('./output/', import.meta.url);
 mkdirSync(out_dir, {recursive: true});
 
 const BASIC = /^[a-z0-9.-]+$/;
 
+function is_basic(norm) {
+	if (BASIC.test(norm)) return true;
+	for (let token of ens_tokenize(norm)) {
+		switch (token.type) {
+			case 'valid':
+			case 'mapped': if (!BASIC.test(String.fromCodePoint(...token.cps))) return false;
+			case 'stop':
+			case 'emoji': continue;
+			default: throw new Error('not normalized');
+		}
+	}
+	return true;
+}
+
 let unique = new Set();
 let non_basic = [];
 for (let label of LABELS) {
 	try {
-		let norm = reference(label);
+		let norm = ens_normalize(label);
 		if (unique.has(norm)) continue;
 		unique.add(norm);	
-		if (!BASIC.test(norm)) {
+		if (!is_basic(norm)) {
 			non_basic.push(norm);
 		}
 	} catch (err) {
