84b1e6a4a491ea76e0abd3c6f89a4c1c729e18f3
Andrew Raffensperger <raffy@me.com>
2022-08-16 16:24:30 -0700

    fix commit

--- eth-labels/db.js
+++ eth-labels/db.js
@@ -1,2 +1,10 @@
 import {readFileSync} from 'node:fs';
-export default JSON.parse(readFileSync(new URL('./db.json', import.meta.url)));
+
+// found through NameRegistered event
+export const REGISTERED = JSON.parse(readFileSync(new URL('./registered.json', import.meta.url)));
+
+// found through reverse records
+export const REVERSE = JSON.parse(readFileSync(new URL('./reverse.json', import.meta.url)));
+
+//export default JSON.parse(readFileSync(new URL('./db.json', import.meta.url)));
+export default [...new Set([REGISTERED, REVERSE].flat().flatMap(s => s.split('.')))];
