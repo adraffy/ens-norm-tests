5db9cb21e72bfa49894319d5746fc02ae2bd8aee
Andrew Raffensperger <raffy@me.com>
2023-02-26 14:59:35 -0800

    update breakdown, uts46 check

--- test-misc/uts46.js
+++ test-misc/uts46.js
@@ -1,15 +1,19 @@
 // find where f(uts46(x)) != f(x)
 
-import LABELS from '../eth-labels/db.js';
+import {import_ens_normalize} from '../impls.js';
+import {read_labels} from '../ens-labels/labels.js';
 import {uts46} from '../impls.js';
-import {ens_normalize} from '@adraffy/ens-normalize';
 import {mkdirSync, writeFileSync} from 'node:fs';
+
+const {ens_normalize} = await import_ens_normalize('dev'); 
+const LABELS = read_labels();
 
 // https://discuss.ens.domains/t/ens-name-normalization/8652/230
 // ENSNORM(UTS46(name)) != ENSNORM(name)
 // 20220801: should only be "xn--*"
 // 20220909: puny literals are no longer allowed, currently 0 results
 // 20220918: still 0 results
+// 20230226: still 0 results
 let errors = test(ens_normalize);
 console.log(errors);
 
