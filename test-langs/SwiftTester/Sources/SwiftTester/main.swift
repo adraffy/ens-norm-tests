9d12a1c4b2dbc3daf4e89552558b54a57cfb5114
Andrew Raffensperger <raffy@me.com>
2025-09-07 12:16:42 -0700

    fix output file size

--- test-langs/SwiftTester/Sources/SwiftTester/main.swift
+++ test-langs/SwiftTester/Sources/SwiftTester/main.swift
@@ -1,33 +1,29 @@
 import ENSNormalize
 import Foundation
 
-@main
-struct Main {
+typealias Rec = [String: String]
 
-    static func main() throws {
-        typealias Rec = [String: String]
-        var recs: [Rec] = []
-        let names = try JSONDecoder().decode(
-            [String].self,
-            from: try Data(
-                contentsOf: URL(fileURLWithPath: "../../ens-labels/labels.json")
-            )
-        )
-        print("Names: \(names.count)")
-        for name in names {
-            var rec: Rec = [:]
-            do {
-                let norm = try name.ensNormalized()
-                if norm != name {
-                    rec["norm"] = norm
-                }
-            } catch let error as NormError {
-                rec["error"] = error.errorDescription
-            }
-            recs.append(rec)
+var recs: [Rec] = []
+let names = try JSONDecoder().decode(
+    [String].self,
+    from: try Data(
+        contentsOf: URL(fileURLWithPath: "../../ens-labels/labels.json")
+    )
+)
+print("Names: \(names.count)")
+
+for name in names {
+    var rec: Rec = [:]
+    do {
+        let norm = try name.ensNormalized()
+        if norm != name {
+            rec["norm"] = norm
         }
-        let data = try JSONEncoder().encode(recs)
-        try data.write(to: URL(fileURLWithPath: "../output/swift.json"))
+    } catch {
+        rec["error"] = error.localizedDescription
     }
-
+    recs.append(rec)
 }
+
+let data = try JSONEncoder().encode(recs)
+try data.write(to: URL(fileURLWithPath: "../output/swift.json"))
